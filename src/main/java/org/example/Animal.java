15097baa9bf61c2f21d44c8bc4f3b153936b176d
YanaDenysenko <yana.denysenko52@gmail.com>
2022-10-26 19:56:49 +0300

    feat: create models

---
+++ src/main/java/org/example/Animal.java
@@ -0,0 +1,12 @@
+package org.example;
+
+abstract public class Animal extends Food{
+    public void eat(Food food){
+        if(this.getClass() == food.getClass()){
+            System.out.println(this.getName() + " can't eat " + food.getName());
+        }
+        else {
+            System.out.println(this.getName() + " eat " + food.getName());
+        }
+    }
+}
